ecbf0fee129a41a96e313db54ebea5e745b4798f
aiyamadeline <aiyaedwards123@gmail.com>
2022-11-04 10:36:21 +0000

    associations created

--- models/index.js
+++ models/index.js
@@ -1,12 +1,24 @@
-const { Cheese, Board, User } = require('./db.js')
-const db = require('./db')
+const Cheese  = require("./models");
+const Board  = require("./models");
+const User  = require("./models");
+
+
 
 // Cheese and Board (many to many)
-
+Cheese.belongsToMany(Board, {through: 'Cheese_Board'})
+Board.belongsToMany(Cheese, {through: 'Cheese_Board'})
 
 
 // User and Cheese (one to many)
-
+User.hasMany(Cheese)
+Cheese.belongsTo(User)
 
 
 // User to Board (one to many)
+User.hasMany(Board)
+Board.belongsTo(User)
+
+
+
+
+module.exports = { Cheese, Book, User }
